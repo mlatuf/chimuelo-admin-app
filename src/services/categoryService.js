b3ef09f729ad9f46a80deb9eb43ce6ac045a45a1
mlatuf <latufmiguel@gmail.com>
2024-09-05 10:17:18 -0300

    fixes on categories

--- src/services/categoryService.js
+++ src/services/categoryService.js
@@ -6,8 +6,7 @@ export const deleteCategory = async (id) => await API.delete(`${CATEGORIES_URL}/
 
 export const getCategory = async (id) => await API.get(`${CATEGORIES_URL}/${id}`);
 
-export const saveCategory = async (payload) =>
-  await API.post(CATEGORIES_URL, { name: payload.name, parent: payload.parent });
+export const saveCategory = async (payload) => await API.post(CATEGORIES_URL, payload);
 
 export const updateCategory = async (id, payload) => await API.patch(`${CATEGORIES_URL}/${id}`, { payload });
 
